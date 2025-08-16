5ded03eddd68a6da78afb77c9599f9db3ca1befd
Misael <britosmisael@gmail.com>
2025-08-16 02:01:43 -0300

    Pointing to a new db

--- app/database.py
+++ app/database.py
@@ -7,6 +7,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+print(os.getenv("DATABASE_URL"))
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
